b847e701c3a87639b6a76fa55fffe3f372215ee7
DrDeath08 <kos.dias98@gmail.com>
2025-09-22 13:02:16 +0500

    0.0001

--- hello/urls.py
+++ hello/urls.py
@@ -5,9 +5,12 @@ about_patterns = [
     path('', views.about, kwargs={"name":"Dias","age":24}),
     re_path('contact', views.contact)
 ]
+anime_patterns = [
+    path('',views.anime)
+]
 
 urlpatterns = [
-    path('anime', views.anime, name='anime'),
     path('', views.index, name='home'),
-    path('about/', include(about_patterns))
+    path('about/', include(about_patterns)),
+    path('anime/', include(anime_patterns))
 ]
