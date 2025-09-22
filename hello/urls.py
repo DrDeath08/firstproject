560cd7bbfcc5976da4ec79790548fbe4f7b7348c
DrDeath08 <kos.dias98@gmail.com>
2025-09-22 11:32:03 +0500

    urls 2

--- hello/urls.py
+++ hello/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from hello import views
 
 urlpatterns = [
-    path(r'^about/contact', views.contact),
-    path(r'^about', views.about, kwargs={"name":"Dias","age":24}),
+    re_path(r'^about/contact', views.contact),
+    re_path(r'^about', views.about, kwargs={"name":"Dias","age":24}),
     path('anime', views.anime, name='anime'),
     path('', views.index, name='home')
 ]
